af6b98beb68bea5005d45e2c7c66487e81aba3ab
Adithyan <adithyanm.22cse@kongu.edu>
2025-02-15 14:35:52 +0530

    nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,33 +3,36 @@ import home from "../assets/home.png"
 import icon from "../assets/icon.png"
 import prog from "../assets/prog2.svg"
 import resume from "../assets/resume1.png"
+import menu from "../assets/menu.png"
 
 const Navbar = () => {
   return (
     <div>
-      <div className="text-white flex justify-between items-center pt-6 ms-[10vw] me-[6vw]">
+      <div className="md:text-white md:flex md:justify-between md:items-center md:pt-6 md:ms-[10vw] md:me-[6vw] text-white flex justify-between items-center pt-6 ms-[10vw] me-[6vw]">
         <div className="flex items-baseline gap-1">
           <h1 className="font-bold text-4xl">A</h1>
           <h1 className="font-bold text-2xl">M</h1>
         </div>
-        <div className="flex justify-center gap-10 items-center text-xl">
-          <div className="flex items-center gap-2 font-style">
-            <img src={home} alt="" className="w-5"/>
+        <div className="md:flex md:justify-center md:gap-10 md:items-center md:text-xl flex justify-center items-center gap-5 text-xl">
+          <div className="md:flex md:items-center md:gap-2 md:font-style flex items-center gap-1 font-style">
+            <img src={home} alt="" className="md:w-5 w-7"/>
             <a href="" className="">Home</a>
           </div>
-          <div className="flex items-center gap-2 font-style">
-            <img src={icon} alt="" className="w-6"/>
+          <div className="md:flex md:items-center md:gap-2 md:font-style flex items-center gap-2 font-style">
+            <img src={icon} alt="" className="md:w-6 w-7"/>
             <a href="" className="">About</a>
           </div>
-          <div className="flex items-center gap-2 font-style">
-            <img src={prog} alt="" className="w-7"/>
+          <div className="md:flex md:items-center md:gap-2 md:font-style flex items-center gap-2 font-style">
+            <img src={prog} alt="" className="md:w-7 w-7"/>
             <a href="" className="">Projects</a>
           </div>
-          <div className="flex items-center gap-2 font-style">
-            <img src={resume} alt="" className="w-5"/>
+          <div className="md:flex md:items-center md:gap-2 md:font-style flex items-center gap-2 font-style">
+            <img src={resume} alt="" className="md:w-5 w-7"/>
             <a href="" className="">Resume</a>
           </div>
-          
+          <div className="md:hidden">
+            <img src={menu} alt="" className="w-6"/>
+          </div>
         </div>
       </div>
     </div>
