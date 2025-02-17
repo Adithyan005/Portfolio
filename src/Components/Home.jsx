46c18a0846fafbcb876e95a08ac4acc230c2b7fd
Adithyan <adithyanm.22cse@kongu.edu>
2025-02-17 23:17:59 +0530

    home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,17 +1,29 @@
 import React from "react";
+import { Typewriter } from 'react-simple-typewriter'
+
 
 const Home = () => {
   return (
     <div className="text-white">
       <div>
         <div className="mt-[25vh] ms-[13rem]">
-            <h1 className="font-style text-4xl">Hi There! 👋</h1>
+          <h1 className="font-style text-4xl">Hi There! 👋</h1>
         </div>
         <div className="mt-[5vh] ms-[13rem] font-style text-5xl">
-            <h1>I'M  <span className="text-purple font-medium">ADITHYAN</span></h1>
+          <h1>
+            I'M <span className="text-purple font-medium">ADITHYAN</span>
+          </h1>
         </div>
         <div className="mt-[9vh] ms-[13rem] text-2xl">
-            <h1>MERN Stack Developer</h1>
+          <Typewriter
+            words={["Mern Stack Developer..."]}
+            loop={Infinity}
+            cursor
+            cursorStyle="|"
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
         </div>
       </div>
     </div>
