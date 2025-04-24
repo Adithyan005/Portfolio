2afdd9d4d37cf8c1c7aee8985c1a0fa124039835
Adithyan <adithyanm.22cse@kongu.edu>
2025-04-24 12:12:10 +0530

    about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,18 +1,24 @@
 import React from "react";
+import about from "../assets/about6.png"
 
 const About = () => {
   return (
     <div className="text-white">
-      <div>
-        <div >
-          <div className="font-main font-medium mt-[7vw] px-5">
+      <div className="flex justify-center items-start">
+        <div className="flex flex-col">
+          <div className="font-main font-medium mt-[10vw] ms-10 w-[50vw] text-center text-2xl">
             <h1>
               Know Who <span className="text-purple">I'M</span>
             </h1>
           </div>
-          <div>
-            <p>Hi Everyone, Iam Adithyan from Salem, Tamil Nadu</p>
+        <div className="w-[45vw] mt-[2vw] ms-10 text-justify text-lg">
+            <p className="">👉Hi Everyone, Iam Adithyan from Salem, Tamil Nadu</p>
+            <p className="py-3">👉Iam Currently Pursuing my Bachelor's Degree in Computer Science Engineering at Kongu Engineering College</p>
+            <p>👉I'm interested in learning new technologies and work on it</p>
           </div>
+        </div>
+        <div className="mt-[4vw]">
+          <img src={about} alt="" className="w-[30vw]"/>
         </div>
       </div>
     </div>
