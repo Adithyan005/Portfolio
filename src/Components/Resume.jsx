a39acb686df87aa92bb54c0c3b7e2dd2762530e3
Adithyan <adithyanm.22cse@kongu.edu>
2025-07-06 01:09:52 +0530

    change-2

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -6,7 +6,7 @@ const Resume = () => {
     <div className='text-white'>
       <div className='flex justify-center items-center pt-12'>
         <a href="/Adithyan_M.pdf" download>
-        <button className='rounded-lg md:px-4 md:py-3 px-5 py-4 text-xl bg-blue'>Download CV</button></a>
+        <button className='rounded-lg md:px-4 md:py-3 px-5 py-4 text-xl md:text-lg font-main font-medium bg-blue'>Download CV</button></a>
       </div>
       <div className='py-12 flex justify-center items-center'>
         <img src={resume} alt="" className='md:w-[50vw] w-[75vw] max-h-fit'/>
